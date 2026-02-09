d7b1c65daf45cd5c0cb4832a4baa11932f9f3fdc
gayeon <lgy3131@naver.com>
2026-02-09 19:33:01 +0900

    refactor: App 컴포넌트 구조 개선 및 useLocation을 사용하여 네비게이션바 조건부 렌더링 로직 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Walk from "./pages/Walk/Walk";
 import Reward from "./pages/Reward/Reward";
@@ -6,7 +6,9 @@ import Mypage from "./pages/Mypage/Mypage";
 import NavBar from "./components/NavBar";
 import MissionDetail from "./pages/Home/Mission/MissionDetail";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+
   // 네비게이션바 숨길 페이지 설정
   const hideNavBarPaths = ["/mission"];
   const hideNavBar = hideNavBarPaths.some((path) =>
@@ -14,23 +16,28 @@ function App() {
   );
 
   return (
-    <BrowserRouter>
-      {/*} 1. 전체 배경 (PC에서 보이는 회색 배경 */}
-      <div className="min-h-dvh bg-gray-100 flex justify-center items-center">
-        {/* 2. 앱 컨테이너 (실제 앱 화면) */}
-        <div className="w-full max-w-md h-dvh bg-background flex flex-col relative">
-          <main className="flex-1 overflow-y-auto relative w-full no-scrollbar">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/mission/:missionId" element={<MissionDetail />} />
-              <Route path="/walk" element={<Walk />} />
-              <Route path="/reward" element={<Reward />} />
-              <Route path="/mypage" element={<Mypage />} />
-            </Routes>
-          </main>
-          {!hideNavBar && <NavBar />}
-        </div>
+    <div className="min-h-dvh bg-gray-100 flex justify-center items-center">
+      {/* 2. 앱 컨테이너 (실제 앱 화면) */}
+      <div className="w-full max-w-md h-dvh bg-background flex flex-col relative">
+        <main className="flex-1 overflow-y-auto relative w-full no-scrollbar">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mission/:missionId" element={<MissionDetail />} />
+            <Route path="/walk" element={<Walk />} />
+            <Route path="/reward" element={<Reward />} />
+            <Route path="/mypage" element={<Mypage />} />
+          </Routes>
+        </main>
+        {!hideNavBar && <NavBar />}
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout />
     </BrowserRouter>
   );
 }
