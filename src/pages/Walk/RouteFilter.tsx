0f31a2523b9e5aa2be4465b7a9ef4fe96164f12d
gayeon <lgy3131@naver.com>
2026-02-12 00:01:22 +0900

    feat : 경로 필터 설정 페이지 UI 구현

--- src/pages/Walk/RouteFilter.tsx
+++ src/pages/Walk/RouteFilter.tsx
@@ -1,9 +1,43 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
+import { PiMapPinFill } from "react-icons/pi";
+import { FaAngleRight } from "react-icons/fa6";
+import ToggleButton from "../../components/ToggleButton";
+import { useState } from "react";
 
 const RouteFilter = () => {
   const navigate = useNavigate();
   const { binId } = useParams();
+
+  const [activeLevel, setActiveLevel] = useState<string>("적당한");
+  const [slopeLevel, setSlopeLevel] = useState<string>("완만한");
+
+  const [isRestingPointOn, setIsRestingPoint] = useState(false);
+  const [isNatureFriendly, setIsNatureFriendly] = useState(false);
+  const [isPedestrianZone, setIsPedestrianZone] = useState(false);
+
+  // 필터 버튼 선택
+  const renderFilterButtons = (
+    options: string[],
+    current: string,
+    setter: (val: string) => void,
+  ) => (
+    <div className="flex gap-3">
+      {options.map((option) => (
+        <button
+          key={option}
+          onClick={() => setter(option)}
+          className={`px-5 py-2 rounded-lg text-sm transition-colors duration-200 ${
+            current === option
+              ? "bg-gray-600 text-white"
+              : "bg-white text-gray-600 border border-gray-500"
+          }`}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 
   // 닫힘
   const handleClose = () => {
@@ -13,16 +47,93 @@ const RouteFilter = () => {
   return (
     // 부모의 지도 위에 덮이는 고정 레이아웃
     <div className="fixed inset-x-0 bottom-0 top-20 bg-white max-w-md mx-auto z-50 rounded-t-3xl flex flex-col overflow-hidden">
-      <div className="px-5.5 pb-5.5 pt-9 flex justify-between items-center border-b border-gray-50">
+      <div className="px-5.5 mt-9 mb-3 flex justify-between items-center">
         <h2 className="text-xl font-bold">AI 맞춤 경로 디자인</h2>
         <button onClick={handleClose} className="p-1">
           <IoClose className="size-8 text-[#52575E]" />
         </button>
       </div>
 
-      <div className="flex-1 overflow-y-auto px-6 pt-6 pb-20 no-scrollbar">
-        {/* 여기에 기획안의 필터 컨텐츠(활동량, 경사도 등)를 넣으세요 */}
-        <p className="text-gray-500 text-sm">목적지: {binId}번 수거함</p>
+      <div className="flex justify-center items-center px-5.5 py-2 mb-4">
+        <div className="w-full px-4.5 py-3 flex justify-between items-center bg-[#F3F7FF] border border-primary rounded-full">
+          <div className="flex gap-1.5 items-center font-semibold">
+            <PiMapPinFill className="text-primary size-5" />
+            <span>나의 목적지</span>
+          </div>
+          <div className="flex gap-1 items-center text-[#202123]">
+            <span>강남구 보건소</span>
+            <FaAngleRight className="size-4" />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5 px-5.5 py-5">
+        {/* 건강 맞춤형 필터 */}
+        <div className="flex flex-col gap-4 mb-4">
+          <h2 className="text-lg font-semibold">건강 맞춤형 필터</h2>
+          <div className="flex flex-col gap-4 mb-5">
+            <div className="flex flex-col gap-1.5">
+              <h5 className="font-medium text-sm">활동량</h5>
+              {renderFilterButtons(
+                ["적당한", "활발한", "최대의"],
+                activeLevel,
+                setActiveLevel,
+              )}
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <h5 className="font-medium text-sm">경사도</h5>
+              {renderFilterButtons(
+                ["완만한", "적당한", "가파른"],
+                slopeLevel,
+                setSlopeLevel,
+              )}
+            </div>
+            <div className="flex justify-between items-center mt-2">
+              <p className="font-medium">휴식 포인트 배치</p>
+              <ToggleButton
+                isOn={isRestingPointOn}
+                onToggle={() => setIsRestingPoint(!isRestingPointOn)}
+              />
+            </div>
+          </div>
+        </div>
+        {/* 환경 맞춤형 필터 */}
+        <div className="flex flex-col gap-4 py-3">
+          <h2 className="text-lg font-semibold">환경 맞춤형 필터</h2>
+          <div className="flex flex-col gap-5">
+            {/* 자연 친화 */}
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="font-medium">자연 친화</p>
+                <p className="text-xs text-gray-500">
+                  녹지율이 높은 경로를 우선순위로 둡니다.
+                </p>
+              </div>
+              <ToggleButton
+                isOn={isNatureFriendly}
+                onToggle={() => setIsNatureFriendly(!isNatureFriendly)}
+              />
+            </div>
+            {/* 보행자 전용 */}
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="font-medium">보행자 전용</p>
+                <p className="text-xs text-gray-500">
+                  보행자 전용 도로의 안전한 길을 추천합니다.
+                </p>
+              </div>
+              <ToggleButton
+                isOn={isPedestrianZone}
+                onToggle={() => setIsPedestrianZone(!isPedestrianZone)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-5.5 fixed bottom-0 pb-12 w-full max-w-md">
+        <button className="w-full bg-primary text-white font-semibold rounded-xl py-4">
+          AI 맞춤 경로 확인하기
+        </button>
       </div>
     </div>
   );
