4acc8ca10b0984353b22f2946235810591cda75d
gayeon <lgy3131@naver.com>
2026-02-18 01:43:50 +0900

    feat : api 기본 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // /api로 시작하는 요청은 백엔드로 전달
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
